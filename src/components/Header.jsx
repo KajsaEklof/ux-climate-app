412aa9e6183fe564ba9b45fd714752e089abe587
Kajsa1 <kajsaKeklof@gmail.com>
2019-12-04 14:38:22 +0000

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,15 @@ import headerImage from "./images/headerImage4.png";
 import "../App.css";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Image from "react-bootstrap/Image";
+import Logo from "./images/logo.png";
+
 
 class Header extends Component {
   state = {};
   render() {
     return (
       <Jumbotron fluid className="jumbo-header">
+      <Image id="logo" src={Logo}/>
         <Image className="header-image" src={headerImage} fluid />
         <h1 className="heading">
           <span>Simple Climate School</span>
