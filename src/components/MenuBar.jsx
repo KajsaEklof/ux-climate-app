7fe381bb30d1ce984e5909b89f3b1581bb1b71bd
Kajsa1 <kajsaKeklof@gmail.com>
2020-01-03 21:13:11 +0100

    added current navigation underline on scroll

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import Logo from "./images/logopink.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { Menu, Sticky } from "semantic-ui-react";
+import Scrollspy from "react-scrollspy";
 
 export default class MenuBar extends Component {
-  state = { activeItem: "home" };
+  state = { activeItem: "" };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  
 
   render() {
     const { activeItem } = this.state;
@@ -15,8 +16,8 @@ export default class MenuBar extends Component {
       <Sticky context={this.props.context}>
         <div className="menubar">
           <Menu pointing secondary>
-            <Menu.Item href="#home" className="menu-logo">
-              <AnchorLink href="#home">
+            <Menu.Item href="#logo" className="menu-logo">
+              <AnchorLink href="#logo">
                 <img
                   src={Logo}
                   height="100%"
@@ -27,58 +28,63 @@ export default class MenuBar extends Component {
             </Menu.Item>
 
             <Menu.Menu position="right">
-              <Menu.Item
-                href="#home"
-                name="home"
-                active={activeItem === "home"}
-                onClick={this.handleItemClick}
+              <Scrollspy className="navlist"
+                items={["home", "co2", "temp", "ice", "sea", "tips"]}
+                currentClassName="active"
               >
-                <AnchorLink href="#home">Home</AnchorLink>
-              </Menu.Item>
-              <Menu.Item
-                href="#co2"
-                name="co2"
-                active={activeItem === "co2"}
-                onClick={this.handleItemClick}
-              >
-                <AnchorLink href="#co2">Emissions</AnchorLink>
-              </Menu.Item>
+                <Menu.Item
+                href="home"
+                  name="home"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                >
+                  <AnchorLink href="#home">Home</AnchorLink>
+                </Menu.Item>
+                <Menu.Item
+                  href="co2"
+                  name="co2"
+                  active={activeItem === "co2"}
+                  onClick={this.handleItemClick}
+                >
+                  <AnchorLink href="#co2">Emissions</AnchorLink>
+                </Menu.Item>
 
-              <Menu.Item
-                href="#temp"
-                name="temp"
-                active={activeItem === "temp"}
-                onClick={this.handleItemClick}
-              >
-                <AnchorLink href="#temp">Temperature</AnchorLink>
-              </Menu.Item>
+                <Menu.Item
+                  href="temp"
+                  name="temp"
+                  active={activeItem === "temp"}
+                  onClick={this.handleItemClick}
+                >
+                  <AnchorLink href="#temp">Temperature</AnchorLink>
+                </Menu.Item>
 
-              <Menu.Item
-                href="#ice"
-                name="ice"
-                active={activeItem === "ice"}
-                onClick={this.handleItemClick}
-              >
-                <AnchorLink href="#ice">Glaciers</AnchorLink>
-              </Menu.Item>
+                <Menu.Item
+                  href="#ice"
+                  name="ice"
+                  active={activeItem === "ice"}
+                  onClick={this.handleItemClick}
+                >
+                  <AnchorLink href="#ice">Glaciers</AnchorLink>
+                </Menu.Item>
 
-              <Menu.Item
-                href="#sea"
-                name="sea"
-                active={activeItem === "sea"}
-                onClick={this.handleItemClick}
-              >
-                <AnchorLink href="#sea">Seas</AnchorLink>
-              </Menu.Item>
+                <Menu.Item
+                  href="#sea"
+                  name="sea"
+                  active={activeItem === "sea"}
+                  onClick={this.handleItemClick}
+                >
+                  <AnchorLink href="#sea">Seas</AnchorLink>
+                </Menu.Item>
 
-              <Menu.Item
-                href="#tips"
-                name="tips"
-                active={activeItem === "tips"}
-                onClick={this.handleItemClick}
-              >
-                <AnchorLink href="#tips">What you can do</AnchorLink>
-              </Menu.Item>
+                <Menu.Item
+                  href="#tips"
+                  name="tips"
+                  active={activeItem === "tips"}
+                  onClick={this.handleItemClick}
+                >
+                  <AnchorLink href="#tips">What you can do</AnchorLink>
+                </Menu.Item>
+              </Scrollspy>
             </Menu.Menu>
           </Menu>
         </div>
